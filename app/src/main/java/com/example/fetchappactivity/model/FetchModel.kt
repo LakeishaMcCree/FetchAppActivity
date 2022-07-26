5fb48ee19d67246e175dc6d53f597ed571c682ba
LakeishaMcCree <lmccree215@icloud.com>
2022-07-26 12:41:46 -0400

    created the data class

---
+++ app/src/main/java/com/example/fetchappactivity/model/FetchModel.kt
@@ -0,0 +1,3 @@
+package com.example.fetchappactivity.model
+
+class FetchModel : ArrayList<FetchModelItem>()
---
+++ app/src/main/java/com/example/fetchappactivity/model/FetchModelItem.kt
@@ -0,0 +1,7 @@
+package com.example.fetchappactivity.model
+
+data class FetchModelItem(
+    val id: Int,
+    val listId: Int,
+    val name: String
+)
